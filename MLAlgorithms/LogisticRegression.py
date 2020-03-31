17c4541d6a38b1ab83dd9c0d2f713555061b0ca8
Faraz Khan <farazkhan138@gmail.com>
2020-03-31 11:25:48 +0530

    Update LogisticRegression.py

--- MLAlgorithms/LogisticRegression.py
+++ MLAlgorithms/LogisticRegression.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn import metrics 
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
+df = pd.read_csv(‘Student-Pass-Fail-Data.csv’)
+df.head()
+
+x = df.drop(‘Pass_Or_Fail’,axis = 1)
+y = df.Pass_Or_Fail
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42)
+
+logistic_regression = LogisticRegression()
